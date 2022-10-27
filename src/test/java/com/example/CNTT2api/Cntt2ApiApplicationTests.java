9e463a897ef44ec8389cf8de27b7e70d484e9358
ButMinhNhat <butminhnhat2000@gmail.com>
2022-10-27 15:11:58 +0700

    set up

---
+++ src/test/java/com/example/CNTT2api/Cntt2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CNTT2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cntt2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
