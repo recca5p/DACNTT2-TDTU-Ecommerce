9607b41ca1447417d64867d6f7d347b5198c67ae
Tan Phat Vo <recca5p@gmail.com>
2022-11-02 11:32:03 +0700

    rebuild repo

---
+++ Ecommerce.Website/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+      <h1>Home page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    data () {
+      return {
+        msg: 'Home page'
+      }
+    }
+  }
+  </script>
