3d45819b1d8f80902eac7ccd193d8d4407b5a09d
ButMinhNhat <butminhnhat2000@gmail.com>
2022-11-30 02:03:33 +0700

    Ec 37 setup axios frontend (#13)
    * setup axios frontend
    
    * setup axios frontend
    
    * setup axios frontend

--- Ecommerce.Api/User/src/main/java/com/cntt2/user/UserApplication.java
+++ Ecommerce.Api/User/src/main/java/com/cntt2/user/UserApplication.java
@@ -3,6 +3,9 @@ package com.cntt2.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,4 +13,13 @@ public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
+
+    @EnableWebMvc
+    public class CorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+    }
+
 }
