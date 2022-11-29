227ec9cad2218a626f7239ee606495b3b8d2a865
ButMinhNhat <butminhnhat2000@gmail.com>
2022-11-30 01:58:17 +0700

    setup axios frontend

--- Ecommerce.Api/Order/src/main/java/com/cntt2/order/OrderApplication.java
+++ Ecommerce.Api/Order/src/main/java/com/cntt2/order/OrderApplication.java
@@ -3,11 +3,22 @@ package com.cntt2.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @EnableWebMvc
+    public class CorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
     }
 }
