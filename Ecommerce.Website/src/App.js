5723f5b0dd63cb7963625fede3f6afc2ec5825c6
ButMinhNhat <butminhnhat2000@gmail.com>
2023-01-11 23:16:39 +0700

    refactor fe code

--- Ecommerce.Website/src/App.js
+++ Ecommerce.Website/src/App.js
@@ -1,17 +1,17 @@
-import AuthLayout from "layout/AuthLayout";
-import HomeLayout from "layout/HomeLayout";
-import CartPage from "pages/Cart Page/CartPage";
-import Home from "pages/home/Home";
-import ProductDetail from "pages/ProductDetail";
-import SignIn from "pages/Sign In";
-import SignUp from "pages/SignUp";
+import AuthLayout from "layout/Auth Layout";
+import HomeLayout from "layout/Home Layout";
+import CartPage from "pages/Cart Page";
+import HomePage from "pages/Home Page";
+import ProductDetail from "pages/Product Detail Page";
+import SignIn from "pages/Sign In Page";
+import SignUp from "pages/Sign Up Page";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
       <Routes>
         <Route element={<HomeLayout />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductDetail />} />
 		  <Route path="/cart" element={<CartPage />} />
           <Route path="*" element={<p>Path not resolved</p>} />
