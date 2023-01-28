2739094322778f9a270e71b6eb653fa1b389e719
ButMinhNhat <butminhnhat2000@gmail.com>
2023-01-29 01:32:01 +0700

    checkout page

---
+++ Ecommerce.Website/src/utils/common.js
@@ -0,0 +1,13 @@
+export const paymentType = {
+	"CREDIT_CARD": "Credit Card",
+	"INTERNATIONAL_BANKING": "International Banking"
+}
+
+export const orderStatus = {
+	"PENDING": "PENDING", 
+	"PAID": "PAID", 
+	"REJECTED": "REJECTED", 
+	"DELIVERING": "DELIVERING", 
+	"CANCELLED": "CANCELLED", 
+	"FINISHED": "FINISHED"
+}
