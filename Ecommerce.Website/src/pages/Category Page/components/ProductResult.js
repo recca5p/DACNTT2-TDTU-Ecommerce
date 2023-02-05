7107139557e1af34af72cb9ab234dac40f15a736
ButMinhNhat <butminhnhat2000@gmail.com>
2023-02-05 20:03:38 +0700

    Fix 0502 (#36)
    * fix bug 0502
    
    * fix bug 0502

--- Ecommerce.Website/src/pages/Category Page/components/ProductResult.js
+++ Ecommerce.Website/src/pages/Category Page/components/ProductResult.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as Actions from 'actions';
 import { Link } from 'react-router-dom';
 import { numberWithCommas } from 'utils/convert';
+import ProductCard from 'components/ProductCard';
 
 const ProductResult = ({ slug }) => {
 	const dispatch = useDispatch();
@@ -28,28 +29,13 @@ const ProductResult = ({ slug }) => {
 				<div className='border-b border-[#ccc] mb-4 mt-2'></div>
 				<div className='grid grid-cols-4 gap-4'>
 					{products.map((item, index) => (
-						<div className="border p-2 max-w-[240px] mx-auto" key={index}>
-							<Link to={`/product/${item.slug}`} >
-								<div className="w-full rounded-lg bg-slate-100">
-									<img
-										src={`${process.env.REACT_APP_API_URL}/image/${item.thumbnail}`} alt="product"
-										className="block w-full h-full object-contain aspect-square
-										hover:scale-110 transition-all ease-out duration-200"
-									/>
-								</div>
-							</Link>
-							<Link 
-								className='block my-2 hover:text-indigo-700 hover:underline line-clamp-3 font-semibold text-gray-700 h-[74px]'
-								to={`/product/${item.slug}`} 
-							>{item.name}</Link>
-							<div className='font-bold text-xl'>{numberWithCommas(item.price)} VND</div>
-						</div>
+						<ProductCard data={item} key={index} className="mx-auto" />
 					))}
 				</div>
 			</>
 			)
 		) : (
-			<div>Loading</div>
+			<div>Loading...</div>
 		)}
 	</div>
   )
