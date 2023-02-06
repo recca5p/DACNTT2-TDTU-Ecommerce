d3854014890b4c4b3f660ad42f782875023fb206
Tan Phat Vo <recca5p@gmail.com>
2023-02-06 07:51:18 +0700

    delete cross orrigin

--- Ecommerce.Api/Image/src/main/java/com/cntt2/image/controller/ImageController.java
+++ Ecommerce.Api/Image/src/main/java/com/cntt2/image/controller/ImageController.java
@@ -18,7 +18,6 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/image")
-@CrossOrigin("*")
 public class ImageController {
     @Autowired
     private ImageService imageService;
